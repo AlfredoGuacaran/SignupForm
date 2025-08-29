9af0000c37cc64492e9cf4c64056aca4b3c49b8b
AlfredoGuacaran <79604260+AlfredoGuacaran@users.noreply.github.com>
2025-08-28 20:58:25 -0400

    Creates form component + styles

---
+++ src/components/SignupForm.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import './SignupForm.css';
+
+const SignupForm: React.FC = () => {
+
+
+  return (
+    <div className="signup-container">
+      <div className="signup-form-wrapper">
+        <h2>Sign Up</h2>
+        <form className="signup-form">
+          {/* Username Field */}
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Enter your username"
+            />
+           <span className="error-message">Error message</span>
+          </div>
+
+          {/* Password Field */}
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+            />
+            <span className="error-message">Error message</span>
+          </div>
+
+          {/* Confirm Password Field */}
+          <div className="form-group">
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              placeholder="Confirm your password"
+            />
+            <span className="error-message">Error message</span>
+          </div>
+
+          {/* Submit Button */}
+          <button 
+            type="submit" 
+            className="submit-button"
+          >
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignupForm;
